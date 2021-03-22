4e170c26c4dae9d929e1b990827328309213cfd8
danasugu <dana.sugu@gmail.com>
2021-03-22 20:13:46 +0200

    install backpack for Laravel 8

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,5 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-file-o'></i> <span>Pages</span></a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('eveniment') }}'><i class='nav-icon la la-file-o'></i> <span>Pages</span></a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('evenimente') }}'><i class='nav-icon la la-file-o'></i> <span>Evenimente</span></a></li>
